1475c56faeb6b02b9a6b3d2fa3f4fa5e3620e6da
Bdgor <129378185+Bdgor@users.noreply.github.com>
2025-07-31 14:15:38 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,30 +1,45 @@
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
+const path = require("path");
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
-  cors: {
-    origin: "*", // дозволити всім
-    methods: ["GET", "POST"]
-  }
+  cors: { origin: "*", methods: ["GET", "POST"] }
 });
 
+app.use(express.static(path.join(__dirname)));
+
 let tower = [];
+let players = {};
+let timer = 900;
+
+// Скидання башти кожні 15 хв
+setInterval(() => {
+  tower = [];
+  io.emit("clear");
+  timer = 900;
+}, 1000 * 900);
+
+// Зменшення таймера кожну секунду
+setInterval(() => {
+  if (timer > 0) timer--;
+  io.emit("tick", { timer });
+}, 1000);
 
 io.on("connection", socket => {
   console.log("Гравець під'єднався:", socket.id);
-  socket.emit("sync", { tower });
+  socket.emit("sync", { tower, timer, scores: players });
 
-  socket.on("add-block", text => {
-    tower.push(text);
-    io.emit("sync", { tower });
-  });
+  socket.on("add-block", ({ word, user }) => {
+    tower.push({ word, user });
 
-  socket.on("clear-tower", () => {
-    tower = [];
-    io.emit("clear");
+    // Підрахунок балів
+    if (!players[user]) players[user] = 0;
+    players[user]++;
+
+    io.emit("sync", { tower, timer, scores: players });
   });
 
   socket.on("disconnect", () => {
