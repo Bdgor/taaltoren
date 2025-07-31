4bbfa7a8c0d166e6fc905b382e0ec1000d0ce773
Bdgor <129378185+Bdgor@users.noreply.github.com>
2025-07-31 15:35:57 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
+const cron = require("node-cron");
 
 const app = express();
 const server = http.createServer(app);
@@ -11,13 +12,19 @@ const io = new Server(server, {
   }
 });
 
-let towers = {}; // окрема вежа для кожного гравця
+let towers = {};
 let scores = {};
 let timer = 900;
 
+// Очищення щодня о 00:00
+cron.schedule("0 0 * * *", () => {
+  scores = {};
+  towers = {};
+  console.log("Щоденне очищення рейтингів і веж");
+});
+
 io.on("connection", socket => {
   console.log("Гравець під'єднався:", socket.id);
-
   socket.on("add-block", ({ word, user }) => {
     if (!towers[user]) towers[user] = [];
     towers[user].push({ word });
@@ -25,28 +32,16 @@ io.on("connection", socket => {
     io.emit("sync", { towers, scores });
   });
 
-  socket.on("sync", ({ towers: newTowers, scores: newScores }) => {
-    towers = newTowers;
-    scores = newScores;
+  socket.on("sync", data => {
+    towers = data.towers;
+    scores = data.scores;
     io.emit("sync", { towers, scores });
   });
+
+  socket.emit("sync", { towers, scores });
 
   socket.on("disconnect", () => {
-    console.log("Гравець вийшов:", socket.id);
-  });
-});
-
-// Таймер для бурі
-setInterval(() => {
-  timer--;
-  io.emit("tick", { timer });
-  if (timer <= 0) {
-    towers = {};
-    timer = 900;
-    io.emit("clear");
-    io.emit("sync", { towers, scores });
-  }
-}, 1000);
+    console
 
 // ✅ Лише один виклик!
 server.listen(3000, () => {
