9c9a0266b1fd746cf6abb7403bd7616b738d1c73
Bdgor <129378185+Bdgor@users.noreply.github.com>
2025-08-02 11:35:31 +0200

    Update server.js

--- server.js
+++ server.js
@@ -123,8 +123,7 @@ io.on("connection", socket => {
 });
 
 // --- Запуск сервера ---
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log("Сервер працює на порті " + PORT);
 });
-
